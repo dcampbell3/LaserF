be9fde56d0f7879beaba5d2d74baea3b9ccc8405
Danny Campbell <daniel.campbell@duke.edu>
2022-11-08 23:42:21 -0500

    rearranged files

--- final_goal_code/final_goal_code.ino
+++ final_goal_code/final_goal_code.ino
@@ -21,11 +21,11 @@ const float laser_spacing = 2 * ball_radius / 3; // inches
 
 //const int num_x_receivers = floor(goal_width / laser_spacing);
 //const int num_y_receivers = floor(goal_height / laser_spacing);
-const int num_x_receivers = 2; 
-const int num_y_receivers = 2; 
+const int num_x_receivers = 3; 
+const int num_y_receivers = 3; 
 
-const int x_pins[num_x_receivers] = {A4, A5}; // stores pins for x receivers in idnex order
-const int y_pins[num_y_receivers] = {A2, A1}; // stores pins for y receivers in index order
+const int x_pins[num_x_receivers] = {A4, A5, A6}; // stores pins for x receivers in idnex order
+const int y_pins[num_y_receivers] = {A2, A1, A0}; // stores pins for y receivers in index order
 
 const int num_samples = 100;   
 
@@ -51,8 +51,12 @@ void assign_val(int n, int curr_x_samples[], int curr_y_samples[], float curr_ti
   //Serial.println(curr_y_samples[0]); 
   samples[n] = Sample{*curr_x_samples, *curr_y_samples, 0.0}; 
   samples[n].time_val = curr_time;
+  samples[n].x_samples[0] = curr_x_samples[0];
+  samples[n].x_samples[1] = curr_x_samples[1];
+  samples[n].x_samples[2] = curr_x_samples[2];
   samples[n].y_samples[0] = curr_y_samples[0];
   samples[n].y_samples[1] = curr_y_samples[1];
+  samples[n].y_samples[2] = curr_y_samples[2];
   //Serial.println(samples[n].y_samples[0]);
 }
 
@@ -62,8 +66,11 @@ Result getResults(Sample samples[], int n, Receiver x_receivers[], Receiver y_re
   
   float time_x1; 
   float time_x2; 
+  float time_x3;
   float time_y1; 
   float time_y2; 
+  float time_y3;
+  
   
   for(int i = 0; i < n; i++){
     if(samples[i].x_samples[0] == 1){
@@ -73,6 +80,9 @@ Result getResults(Sample samples[], int n, Receiver x_receivers[], Receiver y_re
     if(samples[i].x_samples[1] == 1){
       time_x2 = (samples[i].time_val - samples[0].time_val) / 1000;
     }
+    if(samples[i].x_samples[2] == 1){
+      time_x3 = (samples[i].time_val - samples[0].time_val) / 1000;
+    }
     
     if(samples[i].y_samples[0] == 1){
       time_y1 = (samples[i].time_val - samples[0].time_val) / 1000;
@@ -80,27 +90,86 @@ Result getResults(Sample samples[], int n, Receiver x_receivers[], Receiver y_re
     if(samples[i].y_samples[1] == 1){
       time_y2 = (samples[i].time_val - samples[0].time_val) / 1000;
     }
+    if(samples[i].y_samples[2] == 1){
+      time_y3 = (samples[i].time_val - samples[0].time_val) / 1000;
+    }
   }
+
+  
 
   Serial.println("time_x1: " + String(time_x1)); 
   Serial.println("time_y1: " + String(time_y1));
   Serial.println("time_x2: " + String(time_x2));
   Serial.println("time_y2: " + String(time_y2));
+  Serial.println("time_x3: " + String(time_x3));
+  Serial.println("time_y3: " + String(time_y3)); 
+
+  // 1 corresponds to leftmost or bottommost laser
+  // 2 corresponds to rightmost or topmost laser 
+  float x1_location; 
+  float x2_location; 
+  float y1_location; 
+  float y2_location; 
+
+  float time_x1_calc;
+  float time_x2_calc; 
+  float time_y1_calc; 
+  float time_y2_calc; 
+
+  if(time_x1 > time_x3) {
+    x1_location = x_receivers[0].location;
+    x2_location = x_receivers[1].location;
+    time_x1_calc = time_x1;
+    time_x2_calc = time_x2; 
+  }
+  else if(time_x3 > time_x1){
+    x1_location = x_receivers[1].location;
+    x2_location = x_receivers[2].location; 
+    time_x1_calc = time_x2; 
+    time_x2_calc = time_x3; 
+  }
+  else{
+    Serial.println("REDO KICK");
+    x1_location = x_receivers[0].location;
+    x2_location = x_receivers[1].location;
+    time_x1_calc = time_x1;
+    time_x2_calc = time_x2; 
+  }
+
+  if(time_y1 > time_y3) {
+    y1_location = y_receivers[0].location;
+    y2_location = y_receivers[1].location; 
+    time_y1_calc = time_y1;
+    time_y2_calc = time_y2;
+  }
+  else if(time_y3 > time_y1){
+    y1_location = y_receivers[1].location;
+    y2_location = y_receivers[2].location;  
+    time_y1_calc = time_y2;
+    time_y2_calc = time_y3;
+  }
+  else{
+    Serial.println("REDO KICK");
+    y1_location = y_receivers[0].location;
+    y2_location = y_receivers[1].location; 
+    time_y1_calc = time_y1;
+    time_y2_calc = time_y2;
+  }
   
-  float point13[] = {x_receivers[0].location, y_receivers[0].location};
-  Serial.println("Point13: " + String(point13[0]) + "," + String(point13[1])); 
-  float point14[] = {x_receivers[0].location, y_receivers[1].location};
-  Serial.println("Point14: " + String(point14[0]) + "," + String(point14[1]));
-  float point23[] = {x_receivers[1].location, y_receivers[0].location};
-  Serial.println("Point23: " + String(point23[0]) + "," + String(point23[1]));
-  float point24[] = {x_receivers[1].location, y_receivers[1].location};
-  Serial.println("Point24: " + String(point24[0]) + "," + String(point24[1]));
+  float point11[] = {x1_location, y1_location};
+  Serial.println("Point11: " + String(point11[0]) + "," + String(point11[1])); 
+  float point12[] = {x1_location, y2_location};
+  Serial.println("Point12: " + String(point12[0]) + "," + String(point12[1]));
+  float point21[] = {x2_location, y1_location};
+  Serial.println("Point21: " + String(point21[0]) + "," + String(point21[1]));
+  float point22[] = {x2_location, y2_location};
+  Serial.println("Point22: " + String(point22[0]) + "," + String(point22[1]));
   
   // find the center point of the ball with a weighted average
-  ret.x_pos = (point13[0]*time_x1 + point23[0]*time_x2 + point14[0]*time_x1 + point24[0]*time_x2) / (2*time_x1 + 2*time_x2);
-  ret.y_pos = (point13[1]*time_y1 + point23[1]*time_y1 + point14[1]*time_y2 + point24[1]*time_y2) / (2*time_y1 + 2*time_y2);
+  ret.x_pos = (point11[0]*time_x1_calc + point21[0]*time_x2_calc + point12[0]*time_x1_calc + point22[0]*time_x2_calc) / (2*time_x1_calc + 2*time_x2_calc);
+  ret.y_pos = (point11[1]*time_y1_calc + point21[1]*time_y1_calc + point12[1]*time_y2_calc + point22[1]*time_y2_calc) / (2*time_y1_calc + 2*time_y2_calc);
  
-  float* points[] = {point13, point14, point23, point24}; 
+  float* points[] = {point11, point12, point21, point22}; 
   float d = -100000.0; // very small value
   float min_weighted_point[] = {0,0};
   for(int i = 0; i < 4; i++){ 
@@ -110,20 +179,16 @@ Result getResults(Sample samples[], int n, Receiver x_receivers[], Receiver y_re
       min_weighted_point[1] = points[i][1]; 
     }
   }
-//
-//  Serial.println("Min-Point: " + String(min_weighted_point[0]) + "," + String(min_weighted_point[1]));
-//  
   
   float velocity_calc_time;
-  if(min_weighted_point[0] == point13[0] || min_weighted_point[0] == point14[0]){
-    velocity_calc_time = time_x1;
+  if(min_weighted_point[0] == point11[0] || min_weighted_point[0] == point12[0]){
+    velocity_calc_time = time_x1_calc;
   }
-  else if(min_weighted_point[0] == point23[0] || min_weighted_point[0] == point24[0]){
-    velocity_calc_time = time_x2;
+  else if(min_weighted_point[0] == point21[0] || min_weighted_point[0] == point22[0]){
+    velocity_calc_time = time_x2_calc;
   }
   
   ret.velocity = (2*(min_weighted_point[1] - ret.y_pos)) / velocity_calc_time;
-  Serial.println("ret y_pos: " + String(ret.y_pos));
   return ret;
 }
 
@@ -134,8 +199,11 @@ void setup() {
 
   x_receivers[0] = Receiver{'x', 3, 8.0, A4, analogRead(A4) - 10};
   x_receivers[1] = Receiver{'x', 5, 12.0, A5, analogRead(A5) -10};
+  x_receivers[2] = Receiver{'x', 7, 16.0, A6, analogRead(A6) -10}; 
   y_receivers[0] = Receiver{'y', 3, 8.0, A2, analogRead(A2) - 10};
   y_receivers[1] = Receiver{'y', 5, 12.0, A1, analogRead(A1) - 10}; 
+  y_receivers[2] = Receiver{'y', 7, 16.0, A0, analogRead(A0) - 10}; 
+  
   
 //  for (int i = 0; i < num_x_receivers; i++) {
 //    x_receivers[i] = Receiver{'x', i, (i + 1)*laser_spacing, 0};
@@ -192,7 +260,6 @@ void loop() {
       curr_time = millis();
       
       assign_val(iter_count, curr_x_samples, curr_y_samples, curr_time);
-      //samples[iter_count] = {curr_x_samples, curr_y_samples, curr_time};
       
       iter_count++; 
       
